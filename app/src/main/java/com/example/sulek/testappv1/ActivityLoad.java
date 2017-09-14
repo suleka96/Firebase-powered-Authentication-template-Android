5c54008de9dc039f3c3e5475c03430ee160b7055
Suleka Helmini <suleka@omobio.net>
2017-09-14 21:43:23 +0530

    screen loader test logic added

--- app/src/main/java/com/example/sulek/testappv1/ActivityLoad.java
+++ app/src/main/java/com/example/sulek/testappv1/ActivityLoad.java
@@ -1,8 +1,46 @@
 package com.example.sulek.testappv1;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 /**
  * Created by sulek on 11/09/2017.
  */
 
-public class ActivityLoad {
+public class ActivityLoad extends Activity {
+
+    private FirebaseAuth user;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.load);
+
+        user = FirebaseAuth.getInstance();
+
+    }
+
+    public void onStart(View view) {
+        super.onStart();
+        // Check if user is signed in (non-null)
+        FirebaseUser currentUser = user.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(ActivityLoad.this, "Going Home!", Toast.LENGTH_LONG).show();
+            Intent i = new Intent(ActivityLoad.this, ActivityHome.class);
+            startActivity(i);
+        }
+        if(currentUser == null){
+            Toast.makeText(ActivityLoad.this, "Sign in!", Toast.LENGTH_LONG).show();
+            Intent i = new Intent(ActivityLoad.this, ActivitySignin.class);
+            startActivity(i);
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/sulek/testappv1/ActivitySignin.java
+++ app/src/main/java/com/example/sulek/testappv1/ActivitySignin.java
@@ -1,8 +1,10 @@
 package com.example.sulek.testappv1;
 
+import android.app.Activity;
+
 /**
  * Created by sulek on 11/09/2017.
  */
 
-public class ActivitySignin {
+public class ActivitySignin extends Activity {
 }
